85eda22fe414dedc3e397f4655d416482b666324
unknown <tamanna.cse.iubat@gmail.com>
2026-04-14 01:17:13 +0600

    error page design

--- src/assets/Components/ErrorPage/ErrorPage.jsx
+++ src/assets/Components/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>This is 404 not Found</h1>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img
+                    src="https://i.ibb.co.com/B5yr6H7X/404-error.gif"
+                    className="md:max-w-lg rounded-lg shadow-2xl"
+                />
+                <div>
+                    <h1 className="text-2xl  md:text-5xl font-bold">Page Not Found</h1>
+                    <p className="py-6">
+                        Please check Your Internet Connection or Reload
+                    </p>
+                  
+                    <button
+                        className="relative group border-none bg-transparent p-0 outline-none cursor-pointer font-mono font-light uppercase text-base"
+                    >
+                        <span
+                            className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-25 rounded-lg transform translate-y-0.5 transition duration-[600ms] ease-[cubic-bezier(0.3,0.7,0.4,1)] group-hover:translate-y-1 group-hover:duration-[250ms] group-active:translate-y-px"
+                        ></span>
+
+                        <span
+                            className="absolute top-0 left-0 w-full h-full rounded-lg bg-gradient-to-l from-[hsl(217,33%,16%)] via-[hsl(217,33%,32%)] to-[hsl(217,33%,16%)]"
+                        ></span>
+
+                        <div
+                            className="relative flex items-center justify-between py-3 px-6 text-lg text-white rounded-lg transform -translate-y-1 bg-gradient-to-r from-[#f27121] via-[#e94057] to-[#8a2387] gap-3 transition duration-[600ms] ease-[cubic-bezier(0.3,0.7,0.4,1)] group-hover:-translate-y-1.5 group-hover:duration-[250ms] group-active:-translate-y-0.5 brightness-100 group-hover:brightness-110"
+                        >
+                            <span className="select-none"><Link to={'/'}>Go Back</Link></span>
+
+                            <svg
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                className="w-5 h-5 ml-2 -mr-1 transition duration-250 group-hover:translate-x-1"
+                            >
+                                <path
+                                    clip-rule="evenodd"
+                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                    fill-rule="evenodd"
+                                ></path>
+                            </svg>
+                        </div>
+                    </button>
+
+                    
+                </div>
+            </div>
         </div>
     );
 };
--- src/assets/Components/Header/Navbar.jsx
+++ src/assets/Components/Header/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router';
 import './Navbar.css'
+import { ShoppingBag } from 'lucide-react';
 
 const links = <>
     <li><NavLink to={'/'}>Home</NavLink></li>
@@ -47,8 +48,12 @@ const Navbar = () => {
                    {links}
                 </ul>
             </div>
-            <div className="navbar-end">
-                <a className="btn">Sign Up</a>
+            <div className="navbar-end gap-2">
+                <ShoppingBag/>
+                <button className="bg-red-950 text-red-400 border border-red-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+                    <span className="bg-red-400 shadow-red-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+                    Sign Up
+                </button>
             </div>
         </div>
     );
