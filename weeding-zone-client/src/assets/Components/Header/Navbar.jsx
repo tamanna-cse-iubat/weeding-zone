c162ed40810ede029926cca1f326045e64570816
monirzkhan <odvutkabbo@gmail.com>
2026-05-10 21:26:35 +0600

    fix directory issue

--- weeding-zone-client/src/assets/Components/Header/Navbar.jsx
+++ weeding-zone-client/src/assets/Components/Header/Navbar.jsx
@@ -6,7 +6,7 @@ import Swal from 'sweetalert2';
 import { AuthContext } from '../../../Provider/AuthProvider';
 
 const links = <>
-    <li><NavLink to={'/'}>Homeee</NavLink></li>
+    <li><NavLink to={'/'}>Home</NavLink></li>
     <li>
         <details>
             <summary><Link to={'/category/bride'}>Bride</Link></summary>
@@ -58,18 +58,26 @@ const Navbar = () => {
     }
     return (
         <div className="navbar bg-base-100 shadow-sm">
-            <div className="navbar-start">
+            <div className="navbar-start ">
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden p-0">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                     </div>
                     <ul
                         tabIndex="-1"
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        className="menu menu-sm dropdown-content bg-base-100  z-1 mt-3 w-52 p-2 shadow">
                         {links}
+                        <Link to="/wishlist" className="sm:hidden relative group p-2 rounded-full hover:bg-gray-100 transition-colors">
+                    <Heart className="size-6 text-gray-700 group-hover:text-accent transition-colors" />
+                    {wishlist && wishlist.length > 0 && (
+                        <div className="badge badge-accent absolute -top-1 -right-1 size-5 p-0 text-[10px] flex items-center justify-center border-2 border-white">
+                            {wishlist.length}
+                        </div>
+                    )}
+                </Link>
                     </ul>
                 </div>
-                <Link to={'/'}><img src="https://i.ibb.co.com/SzZr84Z/logo.png" alt="" className='md:h-16 h-8' /></Link>
+                <Link to={'/'}><img src="https://i.ibb.co.com/SzZr84Z/logo.png" alt="logo" className='md:h-16 h-8' /></Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
@@ -106,7 +114,7 @@ const Navbar = () => {
                 </div>
 
                 
-                <Link to="/wishlist" className="relative group p-2 rounded-full hover:bg-gray-100 transition-colors">
+                <Link to="/wishlist" className=" relative group p-2 rounded-full hover:bg-gray-100 transition-colors">
                     <Heart className="size-6 text-gray-700 group-hover:text-accent transition-colors" />
                     {wishlist && wishlist.length > 0 && (
                         <div className="badge badge-accent absolute -top-1 -right-1 size-5 p-0 text-[10px] flex items-center justify-center border-2 border-white">
